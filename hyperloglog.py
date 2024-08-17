dbc91be1512e309855dcf6f5a674481dd386828a
wesdoyle <wesdoyle08@gmail.com>
2024-08-17 10:39:32 -0500

    Add notes

--- hyperloglog.py
+++ hyperloglog.py
@@ -5,6 +5,7 @@ relatively small memory footprint.
 
 Links:
 - https://chengweihu.com/hyperloglog/
+- https://redis.io/docs/latest/develop/data-types/probabilistic/hyperloglogs/
 - https://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
 - https://en.wikipedia.org/wiki/HyperLogLog
 
@@ -28,11 +29,40 @@ class HyperLogLog:
     #   Z := 1/(sum from j=1 to m) of (2^-M[j])) "the indicator function"
     #   return  E:= alpha_m * m^2 * Z with alpha_m given by the eqn for harmonic mean
 
-    def __init__(self):
-        pass
+    # From Wikipedia: 
+    # > The HyperLogLog has three main operations: 
+    # > add to add a new element to the set, 
+    # > count to obtain the cardinality of the set and 
+    # > merge to obtain the union of two sets. 
 
-    def add(self):
-        pass
+
+    def __init__(self):
+        self.precision = precision
+        self.m = 1 << precision
+        self.registers = [0] * self.m
+
+    def add(self, v):
+        """
+        Add a new element to the estimator.
+        This hashes the input value and gets two parts of the hash: j and w.
+        j is used to select register, and w contains the remaining bits of the hash. 
+        w is used to potentially update the value at that register.
+        The register is updated with whichever is larger, its current value or the result of _rho(w).
+
+        _rho just calculates the position of the _leftmost_ 1-bit in the binary representation of w.
+        in other words, it is the count of leading zeroes + 1
+
+        we're keeping track of the rarest event we've seen for each register.
+        a long run of leading zeroes is a rare event - longer runs are more rare.
+        by keeping the maximum _rho(w) for each register, 
+        we're essentially recording the rarest event we've 
+        seen for that portion of the hash space.
+
+        :param v: the value to be added to the estimator
+        """
+        x = self._hash(v)
+
+        # ...
 
     def count(self):
         pass
